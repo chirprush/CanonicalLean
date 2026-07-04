83c1380a7d6db7c151bb4a7aebbf9a2b8b6be4a0
chirprush <rush040507@gmail.com>
2026-07-04 17:28:03 -0400

    Initial structure for unary destruct

---
+++ Canonical/Destruct/Unary.lean
@@ -0,0 +1,89 @@
+module
+
+import Lean
+
+open Lean Core Meta
+
+structure Isomorphism where
+  constructors : Array Expr
+  recursor : Expr
+  deriving Inhabited, Repr
+
+def forallTelescopeN
+  (types : List Expr)
+  (k : List (Array Expr) → List Expr → MetaM α) :
+  MetaM α :=
+    match types with
+    | [] => k [] []
+    | t::types' => forallTelescope t fun inputTypes outputType =>
+      forallTelescopeN types' fun inputs outputs => k (inputTypes::inputs) (outputType::outputs)
+
+def recursify (forallType : Expr) (newOutputType : Expr) : MetaM Expr := do
+  forallTelescope forallType fun fvars _ =>
+    mkForallFVars fvars newOutputType
+
+inductive DestructInfo where
+  | trivial
+  | induct (builtinCtors : Array Expr) (builtinRec : Expr)
+  | pi (inputType : Expr) (outputType : Expr)
+  deriving Inhabited, Repr
+
+def extractInfo (t : Expr) : MetaM (Option DestructInfo) := do
+  if !(← inferType t).isSort then return .none
+  let t' ← whnf t
+  match t' with
+  | .const _ _
+  | .app _ _ => do
+    let headName := t'.getAppFn.constName!
+    let headLevels := t'.getAppFn.constLevels!
+    let headArgs := t'.getAppArgs
+    if !(← isInductive headName) then return .none
+    let inductInfo ← getConstInfoInduct headName
+    if inductInfo.isRec || inductInfo.isReflexive then
+      return .some $ DestructInfo.trivial
+    let ctorNames := inductInfo.ctors.toArray
+    let recName := headName ++ `rec
+    return DestructInfo.induct
+      (ctorNames.map fun name => mkAppN (Expr.const name headLevels) headArgs)
+      (mkAppN (Expr.const recName headLevels) headArgs)
+  | .forallE _ inputType outputType _ =>
+    return .some $ DestructInfo.pi inputType outputType
+  | _ =>
+    return .some DestructInfo.trivial
+
+def destructTrivial (t : Expr) : MetaM (Option Isomorphism) := do
+  let ctorTrivial :=
+    Expr.lam `x t (Expr.bvar 0) .default
+  let recTrivial ←
+    withLocalDecl `X .default (Expr.sort (← Meta.mkFreshLevelMVar)) fun fvarX => do
+      withLocalDecl `f .default (Expr.forallE .anonymous t fvarX .default) fun fvarF => do
+        withLocalDecl `x .default t fun fvarInput => do
+          return ← mkLambdaFVars #[fvarX, fvarF, fvarInput] (Expr.app fvarF fvarInput)
+  return .some {
+    constructors := #[ctorTrivial],
+    recursor := recTrivial
+  }
+
+mutual
+def destructInduct (t : Expr) (builtinCtors : Array Expr) (builtinRec : Expr) : MetaM (Option Isomorphism) := do
+  sorry
+
+def destructPi (t : Expr) (inputType : Expr) (outputType : Expr) : MetaM (Option Isomorphism) := do
+  sorry
+
+def destruct (t : Expr) : MetaM (Option Isomorphism) := do
+  if !(← inferType t).isSort then return .none
+  let t ← whnf t
+  let optInfo ← extractInfo t
+  if optInfo.isNone then return .none
+
+  match optInfo.get! with
+  | .trivial => destructTrivial t
+  | .induct builtinCtors builtinRec => destructInduct t builtinCtors builtinRec
+  | .pi inputType outputType => destructPi t inputType outputType
+end
+
+#eval show MetaM Unit from (do
+  let t ← inferType (toExpr (2, 3))
+  IO.println $ repr (← extractInfo t).get!
+  )
