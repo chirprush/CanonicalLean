869b14cb8f7df40b819315416b25866e08de172f
chirprush <rush040507@gmail.com>
2026-07-05 22:54:01 -0400

    Inductive constructors case

--- Canonical/Destruct/Unary.lean
+++ Canonical/Destruct/Unary.lean
@@ -15,8 +15,8 @@ def forallTelescopeN
   MetaM α :=
     match types with
     | [] => k [] []
-    | t::types' => forallTelescope t fun inputTypes outputType =>
-      forallTelescopeN types' fun inputs outputs => k (inputTypes::inputs) (outputType::outputs)
+    | t::types' => forallTelescope t fun inputFVars outputType =>
+      forallTelescopeN types' fun inputs outputs => k (inputFVars::inputs) (outputType::outputs)
 
 def recursify (forallType : Expr) (newOutputType : Expr) : MetaM Expr := do
   forallTelescope forallType fun fvars _ =>
@@ -51,6 +51,19 @@ def extractInfo (t : Expr) : MetaM (Option DestructInfo) := do
   | _ =>
     return .some DestructInfo.trivial
 
+def withDNFProductM (factors : Array Isomorphism) (k : Array Expr → MetaM α) : MetaM (Array α) := do
+  let dnfSize := (factors.map (·.constructors.size)).prod
+  let mut combinations : Array α := Array.emptyWithCapacity dnfSize
+  for i in [:dnfSize] do
+    let mut indices := Array.emptyWithCapacity factors.size
+    let mut j := i
+    for k in [:factors.size] do
+      indices := indices.push (j % factors[k]!.constructors.size)
+      j := j / factors[k]!.constructors.size
+    combinations := combinations.push
+      (← k (indices.mapIdx fun k idx => factors[k]!.constructors[idx]!))
+  return combinations
+
 def destructTrivial (t : Expr) : MetaM (Option Isomorphism) := do
   let ctorTrivial :=
     Expr.lam `x t (Expr.bvar 0) .default
@@ -65,13 +78,28 @@ def destructTrivial (t : Expr) : MetaM (Option Isomorphism) := do
   }
 
 mutual
-def destructInduct (t : Expr) (builtinCtors : Array Expr) (builtinRec : Expr) : MetaM (Option Isomorphism) := do
-  sorry
+partial def destructInduct (t : Expr) (builtinCtors : Array Expr) (builtinRec : Expr) : MetaM (Option Isomorphism) := do
+  let constructors : Option (Array (Array Expr)) := Array.mapM (f := id)
+    (← builtinCtors.mapM fun builtinCtor => do
+    forallTelescope (← inferType builtinCtor) fun inputVars _ => do
+      let isos := (← (← inputVars.mapM inferType).mapM destruct).mapM id
+      if isos.isNone then return .none
+      return .some $ ← withDNFProductM isos.get! fun ctors => do
+        forallTelescopeN (← ctors.mapM inferType).toList fun destructed _ => do
+          let destructed := destructed.toArray
+          let allVars := destructed.flatten
+          let args := destructed.mapIdx fun i vars => mkAppN ctors[i]! vars
+          mkLambdaFVars allVars $ mkAppN builtinCtor args)
+  if constructors.isNone then return .none
+  return .some {
+    constructors := constructors.get!.flatten,
+    recursor := Inhabited.default
+  }
 
-def destructPi (t : Expr) (inputType : Expr) (outputType : Expr) : MetaM (Option Isomorphism) := do
-  sorry
+partial def destructPi (t : Expr) (inputType : Expr) (outputType : Expr) : MetaM (Option Isomorphism) := do
+  return .none
 
-def destruct (t : Expr) : MetaM (Option Isomorphism) := do
+partial def destruct (t : Expr) : MetaM (Option Isomorphism) := do
   if !(← inferType t).isSort then return .none
   let t ← whnf t
   let optInfo ← extractInfo t
@@ -85,5 +113,5 @@ end
 
 #eval show MetaM Unit from (do
   let t ← inferType (toExpr (2, 3))
-  IO.println $ repr (← extractInfo t).get!
+  IO.println $ (← destruct t).get!.constructors
   )
