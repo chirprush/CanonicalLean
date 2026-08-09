c3685f11db768f225737035485a6f78df1713972
chirprush <rush040507@gmail.com>
2026-08-09 19:45:17 -0400

    Implement destructPi (some progress on bug fixes)
    I'll have to ask Chase regarding some of the bugs in the original
    implementation. In particular, the behavior in `types` is really
    confusing me.

--- Canonical/Destruct/Unary.lean
+++ Canonical/Destruct/Unary.lean
@@ -21,6 +21,12 @@ structure Bijection where
   unpack : Array Expr
   deriving Inhabited
 
+def Bijection.p (b : Bijection) : String :=
+  let pack := b.pack
+  let unpack := b.unpack.map fun e => s!"{e}"
+  let unpack' := "\n".intercalate unpack.toList
+  s!"\{\n pack := {pack},\n unpack := [\n  {unpack'}\n ]\n}"
+
 def Bijection.pp (b : Bijection) : MetaM String := do
   let pack ← ppExpr b.pack
   let unpacked ← b.unpack.mapM fun e => do return " " ++ toString (← ppExpr e)
@@ -36,19 +42,46 @@ def applyN (fn : Expr) (args : Array Expr) : Expr :=
   args.foldl (fun app arg => apply app arg) fn
 
 mutual
-def destructTrivial (t : Expr) (binderName : Name) : MetaM Bijection := do
+partial def destructTrivial (t : Expr) (binderName : Name) : MetaM Bijection := do
   let id := Expr.lam binderName t (Expr.bvar 0) .default
   return ⟨id, #[id]⟩
 
-def destructStruct (t : Expr) (binderName : Name)
+partial def destructStruct (t : Expr) (binderName : Name)
   (fields : Nat) (builtinCtor : Expr) : MetaM Bijection := do
-  return Inhabited.default
+  destructTrivial t binderName
 
-def destructPi (t : Expr) (binderName : Name)
+partial def destructPi (t : Expr) (binderName : Name)
   (inputName : Name) (inputType : Expr) (outputType : Expr) (inputInfo : BinderInfo) : MetaM Bijection := do
-  return Inhabited.default
+  let input ← destruct inputType inputName
+  lambdaBoundedTelescope input.pack input.unpack.size fun vars packed => do
+    -- TODO: Is binderName the correct thing to put here? (I think not)
+    let output ← destruct (outputType.instantiate1 packed) binderName
 
-def destruct (t : Expr) (binderName : Name) : MetaM Bijection := do
+    let unpack ← withLocalDecl binderName .default t fun f => do
+      output.unpack.mapM fun field => mkLambdaFVars (#[f] ++ vars) (apply field (f.app packed))
+
+    -- TODO: Chase's implementation maps over output.unpack and gives the
+    -- type for `field` as `field.bindingDomain!.abstract vars`. This cannot be
+    -- correct since if `t` is something like `Nat → Nat`, `types` would
+    -- look like `#[Nat]` instead of `#[Nat → Nat]`, causing `pack` (via `fs`
+    -- below) to look like `fun (x : Nat) (n : Nat) => x n` (definitely wrong).
+    -- Perhaps I could be misinterpreting why `.abstract` was originally used
+    -- above instead of `mkLambdaFVars`?
+    --
+    -- This implementation maps over `unpack` and then takes the output type of
+    -- each projection, which I believe is technically correct but also looks
+    -- very suspicious. Ideally we should be able to do away with `inferType`.
+    let types := unpack.map fun field =>
+      (field.bindingName!, .default, fun _ => do pure (← inferType field).bindingBody!)
+
+    withLocalDecls types fun fs => do
+      let body := applyN output.pack (fs.map (mkAppN · vars))
+      withLocalDecl inputName inputInfo inputType fun var => do
+        let replaced := body.replaceFVars vars (input.unpack.map (apply · var))
+        let pack ← mkLambdaFVars (fs.push var) replaced
+        return ⟨pack, unpack⟩
+
+partial def destruct (t : Expr) (binderName : Name) : MetaM Bijection := do
   match t with
   | Expr.forallE inputName inputType outputType inputInfo =>
     destructPi t binderName inputName inputType outputType inputInfo
@@ -75,8 +108,17 @@ def destruct (t : Expr) (binderName : Name) : MetaM Bijection := do
   | _ => destructTrivial t binderName
 end
 
+structure Bundle (p : Nat → Prop) where
+  value : Nat
+  proof : p value
+
 #eval show MetaM Unit from (do
-  let t := Expr.const `Nat []
-  let b ← destruct t `e
-  IO.println $ ← b.pp
+  let p := Expr.forallE `n (Expr.const `Nat []) (Expr.sort 0) .default
+  let t := Expr.forallE `p p (mkAppN (Expr.const ``Bundle []) #[Expr.bvar 0]) .default
+  IO.println t
+  IO.println $ ← check t
+  -- let t := Expr.forallE `n (Expr.const `Nat []) (Expr.forallE `m (Expr.const `Nat []) (Expr.const `Nat []) .default) .default
+  -- let t := Expr.forallE `n (Expr.const `Nat []) (Expr.const `Nat []) .default
+  let b ← destruct t `x
+  IO.println b.p
 )
